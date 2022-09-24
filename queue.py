5037c090e5aab668f129c044c2bb4dcc063adcd4
Elevenv <vitthaljadhav1420@gmail.com>
2022-09-24 18:15:08 +0530

    basic que operations

---
+++ queue.py
@@ -0,0 +1,33 @@
+maxSize = 4
+
+class Queue:
+
+    def __init__(self):
+        self.queue = []
+    
+    def enqueue(self,item):
+        if len(self.queue)<maxSize:
+            self.queue.append(item)
+        else:
+            print('Queue is full ')
+    
+    def dequeue(self):
+        if len(self.queue)==0:
+            print('Queue is empty')
+        else:
+            ele = self.queue[0]
+            self.queue.pop(0)
+            print(ele,' removed')
+
+    def display(self):
+        print(self.queue)
+
+q = Queue()
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+q.enqueue(4)
+
+q.display()
+q.dequeue()
+q.display()
