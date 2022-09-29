2baaa2b64650f3139b1ad91fe190776a393e2377
Elevenv <vitthaljadhav1420@gmail.com>
2022-09-29 10:57:41 +0530

    Longest palindrome in string

--- LongestPalindrome.py
+++ LongestPalindrome.py
@@ -4,12 +4,11 @@ for i in range(len(string)+1):
     for j in range(i):
         if len(string[j:i])>1:
             stringList.append(string[j:i])
-# pal = []
-# for i in stringList:
-#     if i == i[::-1]:
-#         pal.append(i)
-# d = {}
-# for i in pal:
-#     d[len(i)] = i
-# print(d[max(d)])
-print(stringList)
+pal = []
+for i in stringList:
+    if i == i[::-1]:
+        pal.append(i)
+d = {}
+for i in pal:
+    d[len(i)] = i
+print(d[max(d)])
