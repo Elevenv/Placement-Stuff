0bfa0706be85f37d1142a191611bca11fd6220b9
Elevenv <vitthaljadhav1420@gmail.com>
2022-11-13 00:14:12 +0530

    Fizz buzz leetcode

---
+++ fizzBuzz.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/fizz-buzz/
+
+def fizzBuzz(n):
+    l = []
+    for i in range(1,n+1):
+        if i%3==0 and i%5==0:
+            l.append("FizzBuzz")
+        elif i%3==0:
+            l.append("Fizz")
+        elif i%5==0:
+            l.append("Buzz")
+        else:
+            l.append(str(i))
+    return l
+
+n = 15
+print(fizzBuzz(n))
