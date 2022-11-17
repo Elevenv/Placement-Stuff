abbe664cbc868ee02ce3e6c345a6fe2594c7ccd2
Elevenv <vitthaljadhav1420@gmail.com>
2022-11-17 12:35:31 +0530

    missing number leetcode

--- missingNumber.py
+++ missingNumber.py
@@ -1,13 +1,19 @@
 # https://leetcode.com/problems/missing-number/
 
 
-# nums = [9,6,4,2,3,5,7,0,1]
-nums = [1]
-nums.sort()
+nums = [9,6,4,2,3,5,7,0,1]
+# nums = [1]
+# nums.sort()
 
-for j in range(len(nums)):
-    if j not in nums:
-        print(j)
-        break
-else:
-    print(len(nums))
+# for j in range(len(nums)):
+#     if j not in nums:
+#         print(j)
+#         break
+# else:
+#     print(len(nums))
+
+# OR
+
+nums1 = [i for i in range(len(nums)+1)]
+
+print(sum(nums1)-sum(nums))
