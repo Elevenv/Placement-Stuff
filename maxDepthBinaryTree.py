085b5975b6cea4c6361e25b1a1a6b83e05a46b76
Elevenv <vitthaljadhav1420@gmail.com>
2022-12-01 20:54:31 +0530

    Max depth binary tree

--- maxDepthBinaryTree.py
+++ maxDepthBinaryTree.py
@@ -3,6 +3,9 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+from collections import deque
+
 class Solution:
     def maxDepth(self, root):
         stack = [[root,1]]
@@ -16,6 +19,23 @@ class Solution:
                 stack.append([node.left,depth+1])
                 stack.append([node.right,depth+1])
         return res
+
+    # OR
+
+        # if not root:
+        #     return 0
+        
+        # lvl = 0
+        # queue = deque([root])
+        # while queue:
+        #     for i in range(len(queue)):
+        #         node = queue.popleft()
+        #         if node.left:
+        #             queue.append(node.left)
+        #         if node.right:
+        #             queue.append(node.right)
+        #     lvl+=1
+        # return lvl
 
 if __name__=='__main__':
     root = [3,9,20,None,None ,15,7]
