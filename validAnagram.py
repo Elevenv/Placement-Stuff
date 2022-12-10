2bec83f9fcfd7831e3b214cf9fbee5df5e25a851
Elevenv <vitthaljadhav1420@gmail.com>
2022-12-10 12:53:37 +0530

    valid anagram Leetcode

--- validAnagram.py
+++ validAnagram.py
@@ -8,30 +8,52 @@ t = "nagaram"
 # print(ss)
 # print(tt)
 # print(ss == tt)
+   
 
 # OR
 
+from collections import defaultdict
+
+s = "anagram"
+t = "nagaram"
+
+sd ,td = defaultdict(int),defaultdict(int)
+
 for i in s:
-    if i not in t:
+    sd[i]+=1
+
+for j in t:
+    td[j]+=1
+
+for k in t:
+    if sd[k]!=td[k]:
         print(False)
-        break
 else:
-    d = {}
-    d1 = {}
-    for j in s:
-        if j in d:
-            d[j]+=1
-        else:
-            d[j] = 1
+    print(True)
+
+
+# OR
+
+# for i in s:
+#     if i not in t:
+#         print(False)
+#         break
+# else:
+#     d = {}
+#     d1 = {}
+#     for j in s:
+#         if j in d:
+#             d[j]+=1
+#         else:
+#             d[j] = 1
     
-    for k in t:
-        if k in d1:
-            d1[k]+=1
-        else:
-            d1[k] = 1
-    flag = True
-    for l in d:
-        if d[l]!=d1[l]:
-            flag = False
-    print(flag)
-    
+#     for k in t:
+#         if k in d1:
+#             d1[k]+=1
+#         else:
+#             d1[k] = 1
+#     flag = True
+#     for l in d:
+#         if d[l]!=d1[l]:
+#             flag = False
+#     print(flag)
