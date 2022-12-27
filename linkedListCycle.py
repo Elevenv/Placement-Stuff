99636ebb62f537edb946d4a487ea94f7aebef62d
Elevenv <vitthaljadhav1420@gmail.com>
2022-12-28 00:21:36 +0530

    Linked list cycle

---
+++ linkedListCycle.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # AddressList = []
+        # while head:
+        #     if head not in AddressList:
+        #         AddressList.append(head)
+        #         head = head.next
+        #     else:
+        #         return True
+        # return False
+        
+        slow,fast = head,head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
