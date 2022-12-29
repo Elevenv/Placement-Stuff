bfed730bf134e1be8f42e8d486a68689cec38e53
Elevenv <vitthaljadhav1420@gmail.com>
2022-12-29 10:28:28 +0530

    search 2d matrix LC

---
+++ search2DMatrix2.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/search-a-2d-matrix-ii/description/
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == target:
+                    return True
+        return False
