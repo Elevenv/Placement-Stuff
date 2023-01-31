1c1fa8627704b2ce8049e1264ae41a3a6fda7aa6
Elevenv <vitthaljadhav1420@gmail.com>
2023-01-31 14:59:29 +0530

    flentas sql queries

---
+++ Flentas.sql
@@ -0,0 +1,18 @@
+Task 1:
+
+delete from STUDENTS where COURSE_ID = 'NULL';
+
+
+Task 2:
+
+update STUDENTS set NAME = CONCAT(FISRT_NAME,' ',LAST_NAME);
+
+
+Task 3:
+
+update STUDENTS set FEES = CONCAT('$',FEES);
+
+
+Task 4:
+
+delete from COURSE where NAME = 'NULL'; 
